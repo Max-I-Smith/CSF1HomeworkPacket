e2069c38681f17153bbc739676f5587d49c5f2ea
Max Smith <maxsmith3169@gmail.com>
2021-09-24 15:05:50 -0500

    Fruit Loops Exercise 1 complete

---
+++ CSF1Homework/FruitLoop1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop1
+    {
+        static void Main(string[] args)
+        {
+            string[] fruitNames = {"Apple","Banana","Orange","Lemon","Lime","Peach"};//Initialize array of 6 strings with fruit names
+
+            for (int i = 0; i <fruitNames.Length; i++)
+            {
+                Console.WriteLine(fruitNames[i]);
+            }
+        }
+    }
+}
