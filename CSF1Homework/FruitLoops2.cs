c22f97af30345f4a714b1b660050483c98e74db4
Max Smith <maxsmith3169@gmail.com>
2021-09-24 15:24:17 -0500

    FruitLoops 2 Works

---
+++ CSF1Homework/FruitLoops2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSF1Homework
+{
+
+    class FruitLoops2
+    {
+        static void Main(string[] args)
+        {
+
+            string[] fruitNames = { "Apple", "Banana", "Orange", "Lemon", "Lime", "Peach" };//Initialize array of 6 strings with fruit names
+            string periods = "";
+            foreach (string fruit in fruitNames)
+            {
+                Console.WriteLine(fruit);
+                periods += ".";
+                Console.WriteLine(periods);
+            }//end for 
+        }//end main
+    }//end class
+}//end namespace
